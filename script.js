1d69259ec9e00d7b3a3569286026f2de41512356
DzikriMDP <dzikricahayadialamsyah_2428240126@mhs.mdp.ac.id>
2026-04-14 12:30:42 +0700

    commit 4

--- script.js
+++ script.js
@@ -3,40 +3,42 @@ let jumlah = document.getElementById("jumlah");
 let keterangan = document.getElementById("keterangan");
 
 function simpan() {
-  console.log(nama.value);
-  console.log(jumlah.value);
-  console.log(keterangan.value);
+  console.log(nama.value)
+  console.log(jumlah.value)
+  console.log(keterangan.value)
+
+  localStorage.setItem("nama", nama.value)
+  localStorage.setItem("jumlah", jumlah.value)
+  localStorage.setItem("keterangan", keterangan.value)
 
   if (localStorage.getItem("Barang") === null) {
-    localStorage.setItem("Barang", "[]");
+    localStorage.setItem("Barang", "[]")
   }
-    let Data = JSON.parse(localStorage.getItem("Barang"));
-  console.log(Data);
+    let Data = JSON.parse(localStorage.getItem("Barang"))
+  console.log(Data)
 
   Data.push({
     nama: nama.value,
     jumlah: jumlah.value,
-    keterangan: keterangan.value,
-  });
-  console.log(Data);
+    keterangan: keterangan.value
+  })
+  console.log(Data)
 
-  localStorage.setItem("Barang", JSON.stringify(Data));
-  TambahBarang();
+  localStorage.setItem("Barang", JSON.stringify(Data))
+  TambahBarang()
 }
 
 function TampilBarang() {
-  let hasil = JSON.parse(localStorage.getItem("Barang"));
+  let hasil = JSON.parse(localStorage.getItem("Barang"))
 
   document.getElementById("hasil").innerHTML = ""
 
   hasil.forEach((element) => {
-    document.getElementById("hasil").innerHTML += `<div class="card mb-3">
-        <div class="card-body">
+    document.getElementById("list-barang").innerHTML += `<div class="col-lg-4 col-md-6">
           <h5 class="card-title">${element.nama}</h5>
           <p class="card-text">Jumlah: ${element.jumlah}</p>
           <p class="card-text">Keterangan: ${element.keterangan}</p>
-        </div>
-      </div>`;
+      </div>`
   });
 
 }
