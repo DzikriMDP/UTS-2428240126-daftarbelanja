1860acc7272a84ca3782b40c491dc2ce74b88203
DzikriMDP <dzikricahayadialamsyah_2428240126@mhs.mdp.ac.id>
2026-04-14 12:00:38 +0700

    commit kedua

--- script.js
+++ script.js
@@ -2,4 +2,27 @@ let nama = document.getElementById("nama");
 let jumlah = document.getElementById("jumlah");
 let keterangan = document.getElementById("keterangan");
 
-function T
+function simpan() {
+    console.log(nama.value);
+    console.log(jumlah.value);
+    console.log(keterangan.value);
+
+    if (localStorage.getItem("Barang") === null) {
+        localStorage.setItem("Barang", "[]");
+    }
+
+Data.push({
+    nama: nama.value,
+    jumlah: jumlah.value,
+    keterangan: keterangan.value
+});
+console.log(Data);
+
+localStorage.setItem("Barang", JSON.stringify(Data));
+TambahBarang();
+}
+
+function TampilBarang() {
+    let hasil = JSON.parse(localStorage.getItem("Barang"));
+}
+TampilBarang();
